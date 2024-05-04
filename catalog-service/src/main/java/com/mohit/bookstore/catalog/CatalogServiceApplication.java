629d310510e31c89819756292b144181097d8eca
mkr52 <161114463+mkr52@users.noreply.github.com>
2024-05-04 12:10:10 +0530

    Setup catalog service module

---
+++ catalog-service/src/main/java/com/mohit/bookstore/catalog/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Add spring boot main class here
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+}
